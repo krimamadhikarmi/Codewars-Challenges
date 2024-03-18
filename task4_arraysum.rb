037e4c07016d2093d31a5d972d78b5c5c2dc260a
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-18 17:13:03 +0545

    task4and5

---
+++ task4_arraysum.rb
@@ -0,0 +1,7 @@
+# Sum Numbers
+def sum(numbers)
+    numbers.sum()
+  end
+  
+arr=[1, 5.2, 4, 0, -1]
+puts sum(arr)
---
+++ task5_bool_to_string.rb
@@ -0,0 +1,8 @@
+def bool_to_word bool
+    # TODO
+    if bool==true
+      return "Yes"
+    else
+      return "No"
+    end
+end
