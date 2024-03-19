e45299b218edcca4bd19375598af915414872372
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-19 11:14:38 +0545

    middlechar

--- task8_middlechar.rb
+++ task8_middlechar.rb
@@ -1,9 +1,10 @@
 def getMiddle(name)
    name_len=name.size
    if name_len%2==0
-    return name[(name_len/2)-1,name_len/2]
+     return name[(name_len/2)-1],name[(name_len/2)]
    else
-    return name[name_len/2]
+     return name[name_len/2]
    end
 end
-puts getMiddle("Krima")
+puts getMiddle("chulos")
+
